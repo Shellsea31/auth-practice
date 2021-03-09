6140f16b3bd3e2567c2d78a8ccd35b166e275548
Shellsea <elainesbarrera@gmail.com>
2021-03-09 15:48:27 -0800

    ADDED: update controller

--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -12,6 +12,6 @@ router.get("/", getAll);
 router.post("/", postBlog);
 router.get("/:id", findOne);
 router.delete("/:id", deleteOne);
-router.patch(":id", updateOne)
+router.patch("/:id", updateOne)
 
 module.exports = router;
