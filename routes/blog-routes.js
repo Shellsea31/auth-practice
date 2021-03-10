1265ca9993d0f0b83121e46da448b8819ec6d1be
Shellsea <elainesbarrera@gmail.com>
2021-03-09 16:00:24 -0800

    FIX: refactored blog routes

--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -5,13 +5,20 @@ const {
   postBlog,
   findOne,
   deleteOne,
-  updateOne
+  updateOne,
 } = require("../controllers/BlogController");
 
-router.get("/", getAll);
-router.post("/", postBlog);
-router.get("/:id", findOne);
-router.delete("/:id", deleteOne);
-router.patch("/:id", updateOne)
+// router.get("/", getAll);
+// router.post("/", postBlog);
+
+// condensed
+router.route("/").get(getAll).post(postBlog);
+
+// router.get("/:id", findOne);
+// router.delete("/:id", deleteOne);
+// router.patch("/:id", updateOne);
+
+// condensed
+router.route("/:id").get(findOne).delete(deleteOne).patch(updateOne);
 
 module.exports = router;
