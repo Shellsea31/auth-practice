8ef1b1c0f7d55f60cc362f2eef349870c10ca6b6
Shellsea <elainesbarrera@gmail.com>
2021-03-10 16:34:21 -0800

    ADDED: homepage display blog posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
 import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import WelcomePage from "./Pages/WelcomePage";
 import HomePage from "./Pages/HomePage";
-import AdminPage from "./Pages/AdminPage"
+import AdminPage from "./Pages/AdminPage";
 import ErrorPage from "./Pages/ErrorPage";
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
   const getAllBlogs = async () => {
     const { data } = await axios.get("/blog");
-    console.log(data);
+    setBlogs(data);
   };
 
   useEffect(() => {
@@ -21,8 +22,10 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-           <Route path="/admin" component={AdminPage} />
-          <Route path="/home" component={HomePage} />
+          <Route path="/admin" component={AdminPage} />
+          <Route path="/home">
+            <HomePage blogs={blogs}/>
+          </Route>
           <Route exact path="/" component={WelcomePage} />
           <Route path="/" component={ErrorPage} />
         </Switch>
--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const HomePage = () => {
-    return (
-        <div>
-            <h1>Home</h1>
+const HomePage = (props) => {
+  return (
+    <div className="container">
+      {props.blogs.map((blog) => (
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{blog.title}</h5>
+            <p className="card-text">{blog.text}</p>
+          </div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
